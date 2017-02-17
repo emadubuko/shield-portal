951c9bfacafc2c8a97a887d4a91f471a9686fc3c
cmadubuko <cmadubuko@appzonegroup.com>
2017-02-17 14:56:44 +0100

    error pages

---
+++ DMP/Views/Shared/Error_400.aspx
@@ -0,0 +1,12 @@
+﻿<% Response.StatusCode = 400 %>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>400 Page not found</title>
+</head>
+<body>
+    <h1>400 Page not found</h1>
+</body>
+</html>
---
+++ DMP/Views/Shared/Error_500.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ShieldPortal.Views.Shared
+{
+    public partial class Error_500 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
