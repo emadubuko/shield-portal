f77f53febff37fb70e22ce46e695b1d6fa84789e
Morrison Idiasirue <morrison.idiasirue@gmail.com>
2019-04-26 14:32:39 +0100

    finish hts report for eGSM

---
+++ DMP/Models/eGsmModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShieldPortal.Models
+{
+    public class eGsmModels
+    {
+
+    }
+
+    public class HTSReport
+    {
+        public int HTS_TST { get; set; }
+        public double HTS_TST_POS { get; set; }
+        public string Yield { get; set; }
+        public string Num_Find_Positive { get; set; }
+    }
+
+    public class SDPAggregate
+    {
+        public string SDP_Name { get; set; }
+        public double yield { get; set; } 
+    }
+}
